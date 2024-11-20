a5368a647ae997fda20aa812988123634351dcc0
LegendaryThien <anhthientbd@gmail.com>
2024-11-19 18:34:37 -0800

    feat: Add test paragraph

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ export default function Home() {
             </Head>
             <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
                 <Button>Test</Button>
+                <p>test</p>
             </main>
         </>
     );
